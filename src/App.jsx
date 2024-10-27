90d64b726ff4f2136decaa7f3e34521dd27c0d46
atik-bashar <atik.2108012.ruet.mte@gmail.com>
2024-10-28 00:38:33 +0600

    added recharts

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import DaisyNav from './components/DaisyNav/DaisyNav'
 import LineChart from './components/LineChart/LineChart'
 import NavBar from './components/NavBar/NavBar'
+import Phones from './components/Phones/Phones'
 import PriceOptions from './components/PriceOptions/PriceOptions'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
      <DaisyNav></DaisyNav>
      <PriceOptions></PriceOptions>
      <LineChart></LineChart>
+     <Phones></Phones>
      <h1 className='text-6xl'>Hello</h1>
     </>
   )
